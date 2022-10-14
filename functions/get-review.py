5886edefd3769d997368545be0746fc7c203d3cf
alousada <63957188+alousada@users.noreply.github.com>
2022-10-14 11:03:19 -0300

    Create get-review.py

---
+++ functions/get-review.py
@@ -0,0 +1,19 @@
+#
+#
+# main() will be run when you invoke this action
+#
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# @return The output of this action, which must be a JSON object.
+#
+#
+import sys
+
+def main(dict):
+    return { 'message': 'Hello world' }
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+S
